6c8bb1e3ba754a74b81c67f7f8c3fbfdd595aa32
Sannik8 <sandro.lursmanashvili.1@iliauni.edu.ge>
2022-11-27 00:37:39 +0400

    without javascript
    git commit -m 'without javascript

---
+++ js/gallery.js
@@ -0,0 +1,16 @@
+// const img3 = document.getElementById("img3div");
+// const link3 = document.getElementById("link3")
+
+// img3.onmouseenter = function(){
+//     link3.style.display = "block";
+//     link3.style.position = "absolute";
+//     link3.style.width = "210px"
+//     link3.style.top ="62%";
+//     link3.style.backgroundColor = "#FBFBFB"
+//     link3.style.color = "#000000"
+// }
+// img3.onmouseleave = function(){
+//     link3.style.display = "none";
+// }
+
+
